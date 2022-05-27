cd04b46a466d45ed01d62bb12acfda2aeb223a05
Mahaveer Chouhan <mahi424@users.noreply.github.com>
2022-05-27 13:57:29 +0530

    feature: add ethers and update hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,26 @@
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
-module.exports = {
-  solidity: "0.7.3",
+require("dotenv").config();
+require("@nomiclabs/hardhat-ethers");
+const { API_URL, PRIVATE_KEY } = process.env;
+const config = {
+  solidity: "0.8.1",
+  defaultNetwork: "ropsten",
+  networks: {
+    hardhat: {},
+    ropsten: {
+      url: API_URL,
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts",
+  },
 };
+
+// console.info(config);
+module.exports = config;
